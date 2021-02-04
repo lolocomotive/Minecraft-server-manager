938896d98e4093bb68b938d670685bbbfa537358
lolocomotive <lolocomotive181@gmail.com>
2021-02-04 13:47:50 +0100

    Add Javadoc

--- src/main/java/de/loicezt/srvmgr/ConfigurationHolder.java
+++ src/main/java/de/loicezt/srvmgr/ConfigurationHolder.java
@@ -12,35 +12,74 @@ public class ConfigurationHolder {
     private String jarfile;
     private List<WrapperInstance> servers = new ArrayList<>();
 
-
+    /**
+     * Get the Java executable to use to start the wrapper and the minecraft server
+     *
+     * @return The java executable path
+     */
     public String getJava() {
         return java;
     }
 
+    /**
+     * Set the Java executable to use to start the wrapper and the minecraft server
+     *
+     * @param java the java executable path
+     */
     public void setJava(String java) {
         this.java = java;
     }
 
+    /**
+     * Gets the file path of the wrapper jar
+     *
+     * @return The path of the jar
+     */
     public String getJarfile() {
         return jarfile;
     }
 
+    /**
+     * Sets the jar file path of the wrapper
+     *
+     * @param jarfile tha path of the wrapper jar
+     */
     public void setJarfile(String jarfile) {
         this.jarfile = jarfile;
     }
 
+    /**
+     * Get the servers that the server manager is managing
+     *
+     * @return A {@link List List}<{@link WrapperInstance WrapperInstance}> containing all the servers that the server manager is managing
+     */
     public List<WrapperInstance> getServers() {
         return servers;
     }
 
+    /**
+     * Sets the servers that the server manager is managing
+     *
+     * @param servers A {@link List List}<{@link WrapperInstance WrapperInstance}>  containing all the servers that the server manager is managing
+     */
     public void setServers(List<WrapperInstance> servers) {
         this.servers = servers;
     }
 
+    /**
+     * Gets the type of the node (0 = {@link de.loicezt.srvmgr.master.Master Master},1 = {@link de.loicezt.srvmgr.wrapper.Wrapper Wrapper})
+     *
+     * @return The type of the node
+     */
     public int getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the node (0 = {@link de.loicezt.srvmgr.master.Master Master},1 = {@link de.loicezt.srvmgr.wrapper.Wrapper Wrapper})
+     *
+     * @param type The type of the node
+     */
     public void setType(int type) {
         this.type = type;
     }
