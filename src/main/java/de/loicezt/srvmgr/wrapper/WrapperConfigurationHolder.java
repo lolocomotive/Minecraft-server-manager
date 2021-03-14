69430e658cce8ac6de6e8db525bb51dab330c56e
lolocomotive <lolocomotive181@gmail.com>
2021-03-14 18:22:11 +0100

    Fix javadoc

--- src/main/java/de/loicezt/srvmgr/wrapper/WrapperConfigurationHolder.java
+++ src/main/java/de/loicezt/srvmgr/wrapper/WrapperConfigurationHolder.java
@@ -57,7 +57,7 @@ public class WrapperConfigurationHolder {
      * Gets a list of the paths of the mods to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @return A {@link List}<{@link String}> containing the paths of the mods to add
+     * @return A {@link List}&lt;{@link String}&gt; containing the paths of the mods to add
      */
     public List<String> getMods() {
         return mods;
@@ -67,7 +67,7 @@ public class WrapperConfigurationHolder {
      * Sets a list of the paths of the mods to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @param mods A {@link List}<{@link String}> containing the paths of the mods to add
+     * @param mods A {@link List}&lt;{@link String}&gt; containing the paths of the mods to add
      */
     public void setMods(List<String> mods) {
         this.mods = mods;
@@ -77,7 +77,7 @@ public class WrapperConfigurationHolder {
      * Gets a list of the paths of the worlds to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @return A {@link List}<{@link String}> containing the paths of the worlds to add
+     * @return A {@link List}&lt;{@link String}&gt; containing the paths of the worlds to add
      */
     public List<String> getWorlds() {
         return worlds;
@@ -87,7 +87,7 @@ public class WrapperConfigurationHolder {
      * Sets a list of the paths of the mods to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @param worlds A {@link List}<{@link String}> containing the paths of the mods to add
+     * @param worlds A {@link List}&lt;{@link String}&gt; containing the paths of the mods to add
      */
     public void setWorlds(List<String> worlds) {
         this.worlds = worlds;
@@ -96,7 +96,7 @@ public class WrapperConfigurationHolder {
      * Gets a list of the paths of the additional files to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @return A {@link List}<{@link String}> containing the paths of the additional files such as server.properties,start.sh or eula.txt to add
+     * @return A {@link List}&lt;{@link String}&gt; containing the paths of the additional files such as server.properties,start.sh or eula.txt to add
      */
     public List<String> getAdditionalFiles() {
         return additionalFiles;
@@ -106,7 +106,7 @@ public class WrapperConfigurationHolder {
      * Sets a list of the paths of the additional files to add
      * <p>Will only work if the server is a {@link ServerType#MINIGAME minigame} server</p>
      *
-     * @param additionalFiles A {@link List}<{@link String}> containing the paths of the additional files such as server.properties,start.sh or eula.txt to add
+     * @param additionalFiles A {@link List}&lt;{@link String}&gt; containing the paths of the additional files such as server.properties,start.sh or eula.txt to add
      */
     public void setAdditionalFiles(List<String> additionalFiles) {
         this.additionalFiles = additionalFiles;
